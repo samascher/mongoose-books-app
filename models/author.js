0b18752060e2cab2a4a51184856aa87181446ccf
Sam Ascher <smascher@Sams-MacBook-Air.local>
2017-07-25 14:03:37 -0600

    starting point 2 done

---
+++ models/author.js
@@ -0,0 +1,13 @@
+// models/author.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema({
+  name: String,
+  alive: Boolean,
+  image: String,
+});
+
+var Author = mongoose.model('Author', AuthorSchema);
+
+module.exports = Author;
